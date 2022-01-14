dd4770ba995681e0033e47057b6e5a11f2872a99
Vasil <vasilignatow@gmail.com>
2022-01-14 23:16:10 +0200

    solvet task from Routing exercise

--- JS-Applications/Routing/Exercises/01.Furniture/src/app.js
+++ JS-Applications/Routing/Exercises/01.Furniture/src/app.js
@@ -1,4 +1,6 @@
 import page from '../node_modules/page/page.mjs';
+import { render } from '../node_modules/lit-html/lit-html.js'
+
 import { createPage } from './views/create.js';
 import { dashboardPage } from './views/dashboard.js';
 import { detailsPage } from './views/details.js';
@@ -10,14 +12,21 @@ import { myFurniturePage } from './views/myFurniture.js';
 import * as api from './api/data.js';
 window.api = api;
 
-page('/', dashboardPage);
-page('/create', createPage);
-page('/detail/:id', detailsPage);
-page('/edit/:id', editPage);
-page('/login', loginPage);
-page('/register', registerPage);
-page('/my-furniture', myFurniturePage);
+const main = document.querySelector('.container');
 
+page('/', renderMiddlewere, dashboardPage);
+page('/create', renderMiddlewere, createPage);
+page('/detail/:id', renderMiddlewere, detailsPage);
+page('/edit/:id', renderMiddlewere, editPage);
+page('/login', renderMiddlewere, loginPage);
+page('/register', renderMiddlewere, registerPage);
+page('/my-furniture', renderMiddlewere, myFurniturePage);
 
 page.start();
+
+
+function renderMiddlewere(ctx, next) {
+    ctx.render = (content) => render(content, main);
+    next();
+}
 
