3022dd5a8e5d6e4d23698a00fd291fdaf8363d1e
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2022-09-27 16:09:11 +0300

    Update lab.sql

--- MySQL/Built-in Functions/lab.sql
+++ MySQL/Built-in Functions/lab.sql
@@ -2,3 +2,27 @@
 SELECT `title` 
 FROM `books`
 WHERE SUBSTRING(`title`, 1, 3) = 'The';
+
+#task 2 
+SELECT REPLACE(`title`, 'The', '***') as "title"
+FROM `books`
+WHERE substring(`title`, 1, 3) = 'The';
+
+#task 3
+SELECT round(sum(`cost`),2)
+FROM `books`;
+
+#task 4
+SELECT 
+	CONCAT_WS(' ', `first_name`, `last_name`) AS 'Full Name',
+	TIMESTAMPDIFF(DAY, `born`, `died`) AS 'Days Lived'
+FROM `authors`;
+
+#task 5
+SELECT `title` 
+FROM `books`
+WHERE `title` LIKE 'harry potter%'
+
+SELECT `title` 
+FROM `books`
+WHERE `title` LIKE '^The [a-z]+'
