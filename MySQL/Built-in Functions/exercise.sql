cc69e3c38d77d60d28cf9ba6cf61d21117eccb72
Vasil <vasilignatow@gmail.com>
2022-10-01 17:02:09 +0300

    Added all exercises

--- MySQL/Built-in Functions/exercise.sql
+++ MySQL/Built-in Functions/exercise.sql
@@ -82,3 +82,58 @@ SELECT `first_name`, `last_name`
 FROM `employees`
 WHERE char_length(`last_name`) = 5;
 
+#10. Countries Holding 'A'
+SELECT `country_name`, `iso_code` FROM `countries`
+WHERE `country_name` LIKE '%a%a%a%'
+ORDER BY `iso_code`;
+
+#11. Mix of Peak and River Names
+SELECT 
+	`peak_name`, 
+    `river_name`, 
+	CONCAT(LOWER(`peak_name`), SUBSTRING(LOWER(`river_name`), 2)) AS 'mix' 
+FROM `peaks`, `rivers`
+WHERE RIGHT(`peak_name`, 1) = LEFT(`river_name`, 1)
+ORDER BY `mix`;
+
+#12. Games From 2011 and 2012 Year
+SELECT `name`, date_format(`start`, '%Y-%m-%d') AS `start` FROM `games`
+WHERE YEAR(`start`) IN (2011, 2012)
+ORDER BY `start`, `name`
+LIMIT 50;
+    
+#13. User Email Providers
+SELECT `user_name`, SUBSTRING(`email`, LOCATE('@', `email`) + 1 ) AS `Email Provider` FROM `users`
+ORDER BY `Email Provider`, `user_name`;
+
+#14. Get Users with IP Address Like Pattern
+SELECT `user_name`, `ip_address`FROM `users`
+where `ip_address` LIKE "___.1%.%.___"
+ORDER BY `user_name`;
+
+#15. Show All Games with Duration
+SELECT `name` AS `game`, 
+CASE 
+WHEN HOUR(`start`) >= 0 AND HOUR(`start`) < 12 THEN 'Morning'
+WHEN HOUR(`start`) >= 12 AND HOUR(`start`) < 18 THEN 'Afternoon' 
+ELSE 'Evening'
+END
+AS `Part of the Day`,
+
+CASE 
+WHEN duration <= 3 THEN `Extra Short`
+WHEN duration <= 6 THEN `Short`
+WHEN duration <= 10 THEN `Long`
+ELSE 'Evening'
+END
+AS `Duration`
+FROM `games`;
+
+
+#16. Orders Table
+SELECT 
+	`product_name`,
+	`order_date`,
+    adddate(`order_date`, interval 3 day) AS 'pay_due',
+    adddate(`order_date`, interval 1 month) AS 'deliver_due'
+FROM `orders`;
