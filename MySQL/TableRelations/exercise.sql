65ef8a72dc34122944475bdf083605e24a6cf0d5
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2022-10-05 20:55:08 +0300

    Added exercise

---
+++ MySQL/TableRelations/exercise.sql
@@ -0,0 +1,26 @@
+
+#1.	One-To-One Relationship
+CREATE TABLE `passports`(
+	passport_id INT PRIMARY KEY AUTO_INCREMENT,
+    passport_number VARCHAR(50) UNIQUE
+);
+
+INSERT INTO `passports`(passport_id, passport_number)
+values (101, 'N34FG21B'),  
+	(102, 'K65LO4R7'),
+	(103, 'ZE657QP2');
+    
+CREATE TABLE `person` (
+    person_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) UNIQUE,
+    salary DECIMAL(9 , 2 ),
+    passport_id INT UNIQUE,
+    CONSTRAINT fk_person_passport FOREIGN KEY (`passport_id`)
+        REFERENCES `passports` (`passport_id`)
+);
+    
+INSERT INTO `passports`( first_name, salary, passport_id)
+values ('Roberto', 43300.00, 102),  
+	('Tom', 56100.00, 103),
+	('Yana', 60200.00, 101);
+    
