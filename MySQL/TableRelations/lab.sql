f4c410fa2e06ba7c36e17079af8c634bb61a633e
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2022-10-05 14:11:37 +0300

    Added all tasks from lab

--- MySQL/TableRelations/lab.sql
+++ MySQL/TableRelations/lab.sql
@@ -1,4 +1,5 @@
-# 1.CREATE TABLE mountains(
+# 1. Mountains and Peaks
+CREATE TABLE mountains(
 	`id` INT PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(45) NOT NULL
 );
@@ -26,4 +27,71 @@ CREATE TABLE peaks(
 );
 
 INSERT INTO peaks(`name`, `mountain_id`)
-VALUES('Botev', 1 )
+VALUES('Botev', 1 );
+
+#2. Trip Organization
+SELECT  
+	v.driver_id,
+    v.vehicle_type,
+    concat(c.first_name, ' ', c.last_name) as 'driver_name'
+FROM vehicles as v
+JOIN campers as c
+ON v.driver_id = c.id;
+
+#3.	SoftUni Hiking
+SELECT 
+	r.`starting_point` as 'route_starting_point', 
+	r.`end_point` as 'route_ending_point', 
+    r.`leader_id`, 
+    concat(c.first_name, ' ', c.last_name) as 'leader_name'
+FROM `routes` as r
+JOIN `campers` as c
+ON	r.leader_id = r.id;
+
+SELECT 
+	r.`starting_point` as 'route_starting_point', 
+	r.`end_point` as 'route_ending_point', 
+    r.`leader_id`, 
+    concat(c.first_name, ' ', c.last_name) as 'leader_name'
+FROM `routes` as r, `campers` as c
+WHERE r.leader_id = c.id;
+
+
+#4.	Delete Mountains
+DROP TABLE mountains;
+DROP TABLE peaks;
+
+CREATE TABLE mountains(
+	`id` INT AUTO_INCREMENT PRIMARY KEY, 
+    `name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE peaks (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(50) NOT NULL,
+    `mountain_id` INT,
+    CONSTRAINT fk_peaks_mountains FOREIGN KEY (mountain_id)
+        REFERENCES mountains (id)
+        ON DELETE CASCADE
+);
+
+INSERT INTO `mountains`(`name`) 
+VALUES ('Rila'),
+		('Pirin'),
+		('Old Mountain'),
+		('Vitosha');
+
+INSERT INTO `peaks`(`name`, `mountain_id`) 
+VALUES ('Cherni Vrah', 4),
+		('Kutelo', 2),
+		('Koncheto', 2),
+		('Botev', 3);
+
+
+SELECT * FROM peaks as p
+JOIN mountains as m
+ON p.mountain_id = m.id;
+
+DELETE FROM `mountains` as m
+WHERE m.`name` = 'pirin';
+
