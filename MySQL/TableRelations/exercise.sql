f414462896a907fab6b5f0183ba4acf1e07266c1
Vasil <vasilignatow@gmail.com>
2022-10-07 23:19:47 +0300

    Update exercise.sql

--- MySQL/TableRelations/exercise.sql
+++ MySQL/TableRelations/exercise.sql
@@ -29,7 +29,7 @@ values ('Roberto', 43300.00, 102),
 CREATE DATABASE cars;
 
 CREATE TABLE manufacturers (
-	`manudacturer_id` INT PRIMARY KEY AUTO_INCREMENT,
+	`manufacturer_id` INT PRIMARY KEY AUTO_INCREMENT,
 	`name`VARCHAR(50) NOT NULL UNIQUE,
     `established_on` DATE
     );
@@ -38,6 +38,8 @@ INSERT INTO manufacturers (`name`, `established_on`)
 VALUES 
 	('BMW', '1916/01/03'),
 	('Tesla', '2003/01/01'),
+	('Tesla', '2003/01/01'),
+	('Tesla', '2003/01/01'),
 	('Lada', '1966/01/05');
 
 CREATE TABLE models (
@@ -45,21 +47,158 @@ CREATE TABLE models (
 `name`VARCHAR(70) NOT NULL,
 `manudacturer_id` INT,
 CONSTRAINT fk
-FOREIGN KEY (`manudacturer_id`)
-REFERENCES manufacturers(`manudacturer_id`)
+FOREIGN KEY (`manufacturer_id`)
+REFERENCES manufacturers(`manufacturer_id`)
 );
 
-INSERT INTO manufacturers (`model_id`,`name`,`manudacturer_id`)
+INSERT INTO models (`model_id`,`name`,`manufacturer_id`)
 VALUES 
-	(101),
-	(102),
-	(103),
-	(104),
-	(105),
-	(106);
+	(101, 'X1', 1),
+	(102, 'i6', 1),
+	(103, 'Model S', 2),
+	(104, 'Model X', 2),
+	(105, 'Model 3', 2),
+	(106, 'Nova', 3);
     
- 
+    
+#3.	Many-To-Many Relationship
+CREATE DATABASE `school`;
+
+CREATE TABLE `students`(
+	`student_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(70) NOT NULL
+);
+
+INSERT INTO `students`(`name`)
+VALUES 
+	('Mila'),
+    ('Toni'),
+    ('Ron');
+
+CREATE TABLE `exams`(
+	`exam_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL
+);
+
+ALTER TABLE `exams` AUTO_INCREMENT = 101;
+
+INSERT INTO `exams`(`name`)
+VALUES 
+	('Spring MVC'),
+	('Neo4j'),
+	('Oracle 11g');
 
 
+CREATE TABLE `students_exams`(
+	`student_id` INT NOT NULL,
+    `exam_id` INT NOT NULL,
+    
+    CONSTRAINT pk
+    PRIMARY KEY (`student_id`, `exam_id`),
+    
+	CONSTRAINT fk_students
+    FOREIGN KEY (`student_id`)
+    REFERENCES `students`(`student_id`),
+    
+	CONSTRAINT fk_exams
+    FOREIGN KEY (`exam_id`)
+    REFERENCES `exams`(`exam_id`)
+);
+
+INSERT INTO `students_exams`(`student_id`, `exam_id`)
+VALUES 
+	(1, 101),
+	(1, 102),
+	(2, 101),
+	(3, 103),
+	(2, 102),
+	(2, 103);
 
 
+#4. Self-Referencing
+CREATE DATABASE `selfref`;
+CREATE TABLE `teachers`(
+	`teacher_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+	`manager_id` INT
+);
+
+ALTER TABLE `teachers` AUTO_INCREMENT = 101;
+    
+INSERT INTO `teachers`(`name`, `manager_id`)
+VALUES 
+	('John', NULL),
+	('Maya', '106'),
+	('Silvia', '106'),
+	('Ted', '105'),
+	('Mark', '101'),
+	('Greta', '101');
+    
+ALTER TABLE `teachers`
+ADD CONSTRAINT fk
+FOREIGN KEY (`manager_id`)
+REFERENCES `teachers`(`teacher_id`);
+
+
+#6. University Database
+CREATE DATABASE `university`;
+
+CREATE TABLE `subjects`(
+	`subject_id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+    `subject_name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE `majors`(
+	`major_id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE `payments`(
+	`payment_id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+    `payment_date` DATE NOT NULL,
+    `payment_amount` DECIMAL(8, 2),
+    `student_id` INT(11)
+);
+
+CREATE TABLE `students` (
+	`student_id`  INT(11) PRIMARY KEY AUTO_INCREMENT,
+    `student_number`  VARCHAR(12) NOT NULL UNIQUE,
+    `student_name` VARCHAR(50) NOT NULL,
+    `major_id` INT
+);
+
+CREATE TABLE `agenda` (
+	`student_id`  INT(11) NOT NULL,
+    `subject_id`  INT(11) NOT NULL,
+    CONSTRAINT pk
+    PRIMARY KEY(`student_id`, `subject_id`)
+);
+
+ALTER TABLE `payments`
+ADD CONSTRAINT fk_students
+FOREIGN KEY (`student_id`)
+REFERENCES `students`(`student_id`);
+
+
+ALTER TABLE `students`
+ADD CONSTRAINT fk_major
+FOREIGN KEY (`major_id`)
+REFERENCES `majors`(`major_id`);
+    
+ALTER TABLE `agenda`
+ADD CONSTRAINT fk_subjects
+FOREIGN KEY (`subject_id`)
+REFERENCES `subjects`(`subject_id`);
+
+ALTER TABLE `agenda`
+ADD CONSTRAINT fk_students_agenda
+FOREIGN KEY (`student_id`)
+REFERENCES `students`(`student_id`);
+
+
+    
+    
+    
+    
+    
+    
