65b238e491755542c079f2748e26788bc7ed24a0
Vasil <vasilignatow@gmail.com>
2022-10-08 13:47:15 +0300

    Update exercise.sql

--- MySQL/TableRelations/exercise.sql
+++ MySQL/TableRelations/exercise.sql
@@ -140,6 +140,62 @@ FOREIGN KEY (`manager_id`)
 REFERENCES `teachers`(`teacher_id`);
 
 
+#5. Online Store Database
+CREATE TABLE `item_types`(
+	`item_type_id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE `items`(
+	`item_id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `item_type_id` INT,
+    CONSTRAINT fk
+    FOREIGN KEY (`item_type_id`)
+    REFERENCES  `item_types`(`item_type_id`)
+);
+
+CREATE TABLE `cities`(
+	`city_id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE `customers`(
+	`customer_id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `birthday` DATE NOT NULL,
+    `city_id` INT(11),
+    CONSTRAINT fk_cities
+    FOREIGN KEY (`city_id`)
+    REFERENCES `cities`(`city_id`)
+);
+
+CREATE TABLE `orders`(
+	`order_id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+    `customer_id` INT(11),
+    CONSTRAINT fk_customer
+    FOREIGN KEY (`customer_id`)
+    REFERENCES `customers`(`customer_id`)
+);
+
+CREATE TABLE `order_items`(
+	`order_id` INT(11) NOT NULL,
+    `item_id` INT(11) NOT NULL,
+    CONSTRAINT pk
+    PRIMARY KEY (`order_id`, `item_id`)
+);
+
+ALTER TABLE `order_items`
+ADD CONSTRAINT fk_orders
+FOREIGN KEY (`order_id`)
+REFERENCES `orders`(`order_id`);
+
+ALTER TABLE `order_items`
+ADD CONSTRAINT fk_items
+FOREIGN KEY (`item_id`)
+REFERENCES `items`(`item_id`);
+
+
 #6. University Database
 CREATE DATABASE `university`;
 
@@ -194,11 +250,3 @@ ALTER TABLE `agenda`
 ADD CONSTRAINT fk_students_agenda
 FOREIGN KEY (`student_id`)
 REFERENCES `students`(`student_id`);
-
-
-    
-    
-    
-    
-    
-    
