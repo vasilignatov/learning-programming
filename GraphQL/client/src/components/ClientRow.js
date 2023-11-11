6a944a46ca034eff5d003c8517b0709de8f63d97
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-11-11 17:43:18 +0200

    Added update state functionality

--- GraphQL/client/src/components/ClientRow.js
+++ GraphQL/client/src/components/ClientRow.js
@@ -8,7 +8,19 @@ const ClientRow = ({ client }) => {
 
     const [deleteClient] = useMutation(DELETE_CLIENT, {
         variables: { id: client.id },
-        refetchQueries: [{query: GET_CLIENTS}]
+        // refetchQueries: [{query: GET_CLIENTS}]
+        update(cache, { data: { deleteClient } }) {
+            const { clients } = cache.readQuery({
+                query: GET_CLIENTS
+            });
+            cache.writeQuery({
+                query: GET_CLIENTS,
+                data: {
+                    clients: clients.filter(client => client.id !== deleteClient.id)
+                }
+            })
+        }
+
     });
 
     return (
