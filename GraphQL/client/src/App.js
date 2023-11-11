36ab899305d2c9086b622a07d92916f2c360325f
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-11-11 17:42:40 +0200

    Added routing

--- GraphQL/client/src/App.js
+++ GraphQL/client/src/App.js
@@ -1,21 +1,52 @@
 import Header from './components/Header';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
-import Clients from './components/Clients';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Project from './pages/Project';
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          }
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          }
+        }
+      }
+    }
+  }
+});
 
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
-  cache: new InMemoryCache(),
+  cache
 });
 
 function App() {
   return (
     <>
-      <ApolloProvider client={client}>
+      <BrowserRouter>
 
-        <Header />
-        <Clients />
+        <ApolloProvider client={client}>
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/project/:id" element={<Project />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
 
-      </ApolloProvider>
+        </ApolloProvider>
+
+      </BrowserRouter>
     </>
   );
 }
